79381ebac8e5e22afa8bb752bb6b320b0a988578
ISABELY DA SILVA FERNANDES <CG3026787@car.ifsp>
2025-04-01 09:21:13 -0300

    add projeto lista contatos

---
+++ CARDMOB/ETAPA1/react-exercicio/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ListaContato from './componentes/ListaContato'
+
+function App() {
+  return (
+    <div className="App">
+      <ListaContato />
+    </div>
+  );
+}
+
+export default App;
