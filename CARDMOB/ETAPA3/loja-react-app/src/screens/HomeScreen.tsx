699b376e43ffadfd449a5dabf37fe3db24fe5bb1
belsme <isabelyfernandesz29@gmail.com>
2025-08-19 08:43:08 -0300

    Inclui nova telas: registro e login

--- CARDMOB/ETAPA3/loja-react-app/src/screens/HomeScreen.tsx
+++ CARDMOB/ETAPA3/loja-react-app/src/screens/HomeScreen.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+//import { useNavigation } from "@react-navigation/native"
+import { useTheme } from '../contexts/ThemeContext'; // Nova
 
-import { useTheme } from '../contexts/ThemeContext'
-
-function HomeScreen() {
+function HomeScreen ({ navigation }: any) {
+    //const navigation = useNavigation();
     const { theme, toggleTheme } = useTheme();
     return (
-        <View style= {[ styles.container, {backgroundColor: theme.colors.background} ]}>
-            <Text style={{ color: theme.colors.text, marginBottom: theme.spacing(1) }}>
+        <View style={[styles.container, {backgroundColor: theme.colors.background}]}>
+            <Text style={{color: theme.colors.text, marginBottom: theme.spacing(1) }}>
                 Home Screen
-            </Text>
-            <Button 
-                title="Alternar Tema" 
-                color={theme.colors.primary} 
+                </Text>
+                <Button title="Alternar Tema" color={theme.colors.primary}
                 onPress={toggleTheme}/>
+                <Button title="Ir para detalhes" onPress={ () => navigation.navigate('Details')}/>
+                <Button title="Login" onPress={ () => navigation.navigate ('Login')} />
         </View>
-    )
+    );
 }
 export default HomeScreen;
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 1, 
         justifyContent: 'center',
         alignItems: 'center',
     },
