2f56f360a6c158edfedd9b0e6c6c9491ca966b04
belsme <isabelyfernandesz29@gmail.com>
2025-09-30 08:34:31 -0300

    Tela de catálogo de produtos

--- CARDMOB/ETAPA3/loja-react-app/src/screens/catalog/CatalogScreen.tsx
+++ CARDMOB/ETAPA3/loja-react-app/src/screens/catalog/CatalogScreen.tsx
@@ -1,28 +1,55 @@
-import React, {useContext} from "react";
-import { View, FlatList, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react"; // alterado
+import { View, Text, FlatList, StyleSheet} from 'react-native';
 
 import CatalogCard from "./CatalogCard";
 
-const CatalogScreen = ({navigation}: any)=> {
-    const handleBuyPress = (product : any)=> {
+// Todo: importar o serviço de recuperação do catalog
+import { getCatalog } from '../../services/catalogService'; // novo
+
+import { useShop } from "../../contexts/ShopContext";
+
+const CatalogScreen = ({navigation} : any) => {
+    const [catalog, setCatalog] = useState<any[]>([]); // novo
+    const { addToCart } = useShop();
+
+    // bloco novo
+    useEffect(() => {
+        const fetchCatalog = async () => {
+            try {
+                const data = await getCatalog();
+                setCatalog(data);
+            }
+            catch (error) {
+                console.error('Erro ao buscar o catálogo:', error);
+            }
+        };
+        fetchCatalog();
+        console.log(catalog);
+    }, []);
+
+    const handleBuyPress = (product : any) => {
+        // 1 - Adicionar ao carrinho
+        // 2 - Ir para a tela do carrinho
+        addToCart(product);
         console.log(product);
     };
-const renderItem = ({ product}: any) => (
-    <CatalogCard 
-    product={product}
-    onBuyPress={()=> handleBuyPress(product)}
-    />
-);
 
-return(
-    <View style={styles.container}>
-        <FlatList
-        data={[]}
-        renderItem={renderItem}
-        keyExtractor={(item:any) => item.id}        
-    />
-    </View>
+    const renderItem = ({ item }: any) => ( // alterado
+        <CatalogCard 
+            product={item} // alterado
+            onBuyPress={() => handleBuyPress(item)} // alterado
+        />
+    );
 
+    return (
+        <View style={styles.container}>
+            <Text>Menu</Text>
+            <FlatList 
+                data={catalog} // alterado
+                renderItem={renderItem}
+                keyExtractor={(item: any) => item.id.toString()} // alterado
+            />
+        </View>
     );
 };
 
