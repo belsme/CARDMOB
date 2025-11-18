c33960939f986fdd3d7e2d27e67dee1bbcfb4c66
belsme <isabelyfernandesz29@gmail.com>
2025-11-18 08:10:57 -0300

    Conclusão da tela de checkout

--- ETAPA3/loja-react-app/src/contexts/ShopContext.tsx
+++ ETAPA3/loja-react-app/src/contexts/ShopContext.tsx
@@ -3,12 +3,17 @@ import React, { createContext, useContext, useState } from "react";
 type ShopContextType = {
     cartItems: any[];
     addToCart: (item: any) => Promise<void>;
+    removeFromCart: (itemId: number) => Promise<void>;
+    getTotalPrice: () => number;
+    clearCart: () => void;
+    lastOrderInfo: (orderInfo: any) => void;
 };
 
 export const ShopContext = createContext<ShopContextType>({} as ShopContextType);
 
 export const ShopProvider: React.FC<{ children: React.ReactNode}> = ({ children}) => {
     const [cartItems, setCartItems] = useState<any[]>([]);
+    const [orderInfo, setOrderInfo] = useState<any[]>([]);
 
     const addToCart = async (item: any, quantity: number = 1) => {
         setCartItems(prevItems => {
@@ -30,13 +35,34 @@ export const ShopProvider: React.FC<{ children: React.ReactNode}> = ({ children}
         )
     }
 
+    const removeFromCart = (itemId: number) => {
+        setCartItems((prevItems) =>  
+            prevItems.filter(item => item.id !== itemId)
+        );
+    }
+
+    const getTotalPrice = () => {
+        return cartItems.reduce(
+            (total, item) => total + item.price * item.quantity, 0
+        ).toFixed(2);
+    }
+
+    const clearCart = () => {
+        setCartItems([]);
+    }
+
+    const lastOrderInfo = (orderInfo: any) => {
+        setOrderInfo(orderInfo);
+    }
+
     return (
         <ShopContext
-            value={ { cartItems, addToCart } }
-        >
+        value={ { cartItems, addToCart, removeFromCart, getTotalPrice, clearCart, orderInfo, lastOrderInfo } }>
+           
             {children}
         </ShopContext>
     );
 }
 
 export const useShop = () => useContext(ShopContext);
+
