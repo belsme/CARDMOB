56f27a2f043daa7537c9526b70c2914f12985356
belsme <isabelyfernandesz29@gmail.com>
2025-11-18 10:16:52 -0300

    Extrai dados do usuário do token

--- ETAPA3/loja-react-app/src/contexts/AuthContext.tsx
+++ ETAPA3/loja-react-app/src/contexts/AuthContext.tsx
@@ -1,11 +1,16 @@
 import React, { createContext, useState, useEffect, useContext} from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import { getTokenData } from "../services/authService"; // novo.
+
 type AuthContextType = {
     user: { token: string } | null;
     login: (token: string) => Promise<void>;
     logout: () => Promise<void>;
     loading: boolean;
+
+    getUserDataFromToken: (token: string | null) => Promise<any[]>; // novo
+    userData: Promise<any[]>; // novo
 };
 const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
@@ -13,6 +18,8 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     // Lógica do context provider.
     const [user, setUser] = useState<{ token: string } | null>(null);
     const [loading, setLoading] = useState(true);
+
+    const [userData, setUserData] = useState<any[]>([]); // novo
 
     useEffect( () => {
         const loadUser = async () => {
@@ -23,6 +30,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
             setLoading(false);
         };
         loadUser();
+        getUserDataFromToken(); // novo
     }, []);
 
     const login = async (token: string) => {
@@ -34,10 +42,17 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
         await AsyncStorage.removeItem('token');
         setUser(null);
     }
+      // novo callback.
+      const getUserDataFromToken = async () => {
+        const token = await AsyncStorage.getItem('token');
+        const tokenData = getTokenData(token);
+        setUserData(tokenData);
+    }
+
 
     return (
         <AuthContext 
-            value={{ user, login, logout, loading }}
+        value={{ user, login, logout, loading, userData }} 
         >
             {children}
         </AuthContext>
